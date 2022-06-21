6a52e0338d8f7c93f83687c8ae3785116cb66903
matheus costa <matheuscosta.tech@gmail.com>
2022-06-21 08:06:16 -0300

    Connecting with graphcms

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import { useState } from 'react'
+import { gql, useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react'
+import { client } from './lib/apollo';
 
-function App() {
+const GET_LESSONS_QUERY = gql`
+	query {
+		lessons {
+			id
+			title
+		}
+	}
+`
 
-  return <h1 className='text-2xl'>Hello world</h1>;
+interface Lesson {
+	id: string;
+	title: string;
 }
 
-export default App
+function App() {
+	// Old implementation:
+	//useEffect(() => {
+	//	client.query({
+	//		query: GET_LESSONS_QUERY
+	//	}).then(res => {
+	//		console.log(res.data);
+	//	});
+	//}, []);
+	// New implementation:
+	const { data } = useQuery(GET_LESSONS_QUERY);
+	
+  return <ul>
+		{data?.lessons.map((lesson: Lesson) => {
+			return <li key={lesson.id}>{lesson.title}</li>
+		})}
+	</ul>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { client } from './lib/apollo';
 import './styles/globals.css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
