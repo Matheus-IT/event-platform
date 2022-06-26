e9e0471dce02100d0075a594fdc8dee15d451f90
matheus costa <matheuscosta.tech@gmail.com>
2022-06-26 07:43:46 -0300

    Getting first class by default

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,20 +1,44 @@
-import { useParams } from "react-router-dom";
-import { Header } from "../components/Header";
-import { Sidebar } from "../components/Sidebar";
-import { Video } from "../components/Video";
+import { useParams } from 'react-router-dom';
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+import { Video } from '../components/Video';
+import { GetLessonsQuery, useGetLessonsQuery } from '../graphql/generated';
+
+interface Lesson {
+	id: string;
+	title: string;
+	slug: string;
+	availableAt: Date;
+	type: 'live' | 'class';
+}
+
+function adaptToMyLessons(query: GetLessonsQuery) {
+	let myLessons = [];
+	for (let lesson of query.lessons) {
+		myLessons.push({
+			id: lesson.id,
+			title: lesson.title,
+			slug: lesson.slug,
+			availableAt: lesson.availableAt,
+			type: lesson.lessonType,
+		});
+	}
+	return myLessons;
+}
 
 export function Event() {
-  const { slug } = useParams<{ slug: string }>();
+	const { data } = useGetLessonsQuery();
+	const { slug: currentLessonSlug } = useParams<{ slug: string }>();
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex flex-1">
-        {slug
-          ? <Video lessonSlug={slug} />
-          : <div className="flex-1"></div>}
-        <Sidebar />
-      </main>
-    </div>
-  );
+	const lessons: Lesson[] = adaptToMyLessons(data ?? { lessons: [] });
+
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<main className="flex flex-1">
+				<Video lessonSlug={currentLessonSlug ?? ''} />
+				{data ? <Sidebar lessons={lessons} /> : <div className="flex flex-1"></div>}
+			</main>
+		</div>
+	);
 }
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,70 +1,77 @@
-import { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Logo } from "../components/Logo";
-import { useCreateSubscriberMutation } from "../graphql/generated";
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Logo } from '../components/Logo';
+import { useCreateSubscriberMutation, useGetLessonsQuery } from '../graphql/generated';
 
 export function Subscribe() {
-  const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+	const navigate = useNavigate();
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
 
-  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
+	const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
-  async function handleSubscribe(event: FormEvent) {
-    event.preventDefault();
+	const { data } = useGetLessonsQuery();
+	const firstLesson = data?.lessons[0];
 
-    await createSubscriber({
-      variables: {
-        name,
-        email,
-      }
-    });
+	async function handleSubscribe(event: FormEvent) {
+		event.preventDefault();
 
-    navigate('/event');
-  }
+		await createSubscriber({
+			variables: {
+				name,
+				email,
+			},
+		});
 
-  return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
+		navigate(`/event/lesson/${firstLesson?.slug}`);
+	}
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
-          </h1>
+	return (
+		<div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+			<div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+				<div className="max-w-[640px]">
+					<Logo />
 
-          <p className="mt-4 text-gray-200 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quo officia pariatur a ex dicta beatae tempore exercitationem at eius! Beatae ad harum, reprehenderit</p>
-        </div>
+					<h1 className="mt-8 text-[2.5rem] leading-tight">
+						Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com
+						<strong className="text-blue-500">React</strong>
+					</h1>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+					<p className="mt-4 text-gray-200 leading-relaxed">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quo officia pariatur a ex dicta beatae
+						tempore exercitationem at eius! Beatae ad harum, reprehenderit
+					</p>
+				</div>
 
-          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-            <input
-              className="bg-gray-900 rounded px-5 h-14"
-              type="text"
-              placeholder="Digite seu nome completo"
-              onChange={(event => setName(event.target.value))}
-            />
-            <input
-              className="bg-gray-900 rounded px-5 h-14"
-              type="email"
-              placeholder="Digite seu e-mail"
-              onChange={event => setEmail(event.target.value)}
-            />
+				<div className="p-8 bg-gray-700 border border-gray-500 rounded">
+					<strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
-            <button
-              type="submit"
-              disabled={loading}
-              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
-            >
-              Garantir minha vaga
-            </button>
-          </form>
-        </div>
-      </div>
-        
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="Hero" />
-    </div>
-  );
+					<form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+						<input
+							className="bg-gray-900 rounded px-5 h-14"
+							type="text"
+							placeholder="Digite seu nome completo"
+							onChange={(event) => setName(event.target.value)}
+						/>
+						<input
+							className="bg-gray-900 rounded px-5 h-14"
+							type="email"
+							placeholder="Digite seu e-mail"
+							onChange={(event) => setEmail(event.target.value)}
+						/>
+
+						<button
+							type="submit"
+							disabled={loading}
+							className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+						>
+							Garantir minha vaga
+						</button>
+					</form>
+				</div>
+			</div>
+
+			<img src="/src/assets/code-mockup.png" className="mt-10" alt="Hero" />
+		</div>
+	);
 }
