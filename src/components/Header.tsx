83e46b8d2c026b696978f37aeedc97121a55c934
matheus costa <matheuscosta.tech@gmail.com>
2022-06-26 07:45:31 -0300

    Fixing header height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { Logo } from "./Logo";
+import { Logo } from './Logo';
 
 export function Header() {
 	return (
-		<header className="w-full py-20 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+		<header className="w-full py-6 flex items-center justify-center bg-gray-700 border-b border-gray-600">
 			<Logo />
 		</header>
 	);
